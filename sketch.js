70625da999e8e260f53fff03abe2c83356f1e12e
PeterShak <peter_shaknes@avenues-ny.org>
2025-04-15 09:20:12 -0400

    Update class to use vectors where possible

--- sketch.js
+++ sketch.js
@@ -1,29 +1,40 @@
-let colorlist = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'];
+let colorlist = [
+  '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6',
+  '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3',
+  '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'
+];
 
 let movers = [];
-let magnets = []; // Array to store the magnetic balls
-let G = 0.1;  
-let wind = 0.1; 
-let magnetStrength = 20;  
+let magnets = [];
+let G = 0.1;
+let wind = 0.1;
+let magnetStrength = 20;
 
 function setup() {
   createCanvas(400, 400);
+  ellipseMode(RADIUS);
 
   for (let i = 0; i < 10; i++) {
     movers.push(
-      new Mover(random(width), random(height), random(-1, 1), random(-1, 1), 20, random(colorlist))
+      new Mover(
+        random(width),
+        random(height),
+        random(-1, 1),
+        random(-1, 1),
+        20,
+        color(random(colorlist))
+      )
     );
   }
-
-  ellipseMode(RADIUS);
 }
 
 function draw() {
   background(220);
 
+  // Apply magnetic force from each magnet to each mover
   for (let magnet of magnets) {
     for (let mover of movers) {
-      applyMagneticForce(magnet.x, magnet.y, mover);
+      applyMagneticForce(magnet, mover);
     }
   }
 
@@ -31,28 +42,26 @@ function draw() {
     mover.update();
   }
 
+  // Draw magnets
   for (let magnet of magnets) {
-    magnet.display();
+    magnet.display(); 
   }
 }
 
-// Create a magnet ball at the mouse position when clicked
+// Click to add a new magnet
 function mousePressed() {
-  magnets.push(new Mover(mouseX, mouseY, 0, 0, 30, color(0, 0, 255))); 
+  let m = new Mover(mouseX, mouseY, 0, 0, 30, color(0, 0, 255));
+  m.isMagnet = true;
+  magnets.push(m);
 }
 
-// Apply magnetic force to a moving ball from a stationary magnet
-function applyMagneticForce(mx, my, mover) {
-  let dx = mx - mover.x; 
-  let dy = my - mover.y; 
-  let distance = Math.sqrt(dx * dx + dy * dy); 
+// Magnetic force from magnet to mover
+function applyMagneticForce(magnet, mover) {
+  let force = p5.Vector.sub(magnet.position, mover.position);
+  let distance = force.mag();
 
   if (distance > 0) {
-    
-    let force = magnetStrength / distance;
-
-    let angle = Math.atan2(dy, dx); 
-    mover.dx += force * Math.cos(angle); 
-    mover.dy += force * Math.sin(angle); 
+    force.setMag(magnetStrength / distance);
+    mover.velocity.add(force);
   }
 }
