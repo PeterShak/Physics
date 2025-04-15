cdcb9725318aede8ec3f6eb39b8c5399fe00af65
PeterShak <peter_shaknes@avenues-ny.org>
2025-04-15 10:56:07 -0400

    Added friction again
    Didn't paste the right thing the first time

--- sketch.js
+++ sketch.js
@@ -1,69 +1,72 @@
-let colorlist = [
-  '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6',
-  '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3',
-  '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'
-];
+class Mover {
+  constructor(x, y, dx, dy, r, c) {
+    this.position = createVector(x, y);
+    this.velocity = createVector(dx, dy);
+    this.acceleration = createVector(0, 0); 
+    this.r = r;
+    this.c = c;
+    this.isMagnet = false; 
+    this.frictionCoefficient = 0.01; // smaller number means more slippery
+  }
 
-let movers = [];
-let magnets = [];
-let G = 0.1;   
-let wind = 0.1;    
-let magnetStrength = 20;
-
-function setup() {
-  createCanvas(400, 400);
-  ellipseMode(RADIUS);
-
-  
-  for (let i = 0; i < 10; i++) {
-    movers.push(
-      new Mover(
-        random(width),
-        random(height),
-        random(-1, 1),
-        random(-1, 1),
-        20,
-        color(random(colorlist))
-      )
-    );
+  applyFriction() {
+  if (this.velocity.mag() > 0.01) { 
+    let friction = this.velocity.copy(); 
+    friction.normalize();              
+    friction.mult(-1);                
+    friction.mult(this.frictionCoefficient); 
+    this.applyForce(friction);        
   }
 }
 
-function draw() {
-  background(220);
+  applyForce(force) {
+    this.acceleration.add(force); 
+  }
 
-  // Apply magnetic force from each magnet to each mover
-  for (let magnet of magnets) {
-    for (let mover of movers) {
-      applyMagneticForce(magnet, mover);
+  update() {
+    if (!this.isMagnet) {
+      
+      this.applyForce(createVector(0, G));
+      this.applyForce(createVector(wind, 0));
+      
+      this.applyFriction();
+
+      this.velocity.add(this.acceleration);
+      this.position.add(this.velocity);
+
+      this.acceleration.mult(0);
+    }
+
+    this.containWithinWindow();
+    this.draw();
+  }
+
+  draw() {
+    fill(this.c);
+    circle(this.position.x, this.position.y, this.r);
+  }
+
+  containWithinWindow() {
+    if (this.position.x < this.r) {
+      this.position.x = this.r;
+      this.velocity.x *= -1;
+    }
+    if (this.position.x > width - this.r) {
+      this.position.x = width - this.r;
+      this.velocity.x *= -1;
+    }
+    if (this.position.y < this.r) {
+      this.position.y = this.r;
+      this.velocity.y *= -1;
+    }
+    if (this.position.y > height - this.r) {
+      this.position.y = height - this.r;
+      this.velocity.y *= -1;
     }
   }
 
-  // Update all movers
-  for (let mover of movers) {
-    mover.update();
-  }
-
-  // Draw magnets
-  for (let magnet of magnets) {
-    magnet.display();
-  }
-}
-
-// When mouse is pressed, create a new magnet at the mouse position
-function mousePressed() {
-  let m = new Mover(mouseX, mouseY, 0, 0, 30, color(0, 0, 255));
-  m.isMagnet = true;
-  magnets.push(m);
-}
-
-// Apply magnetic force from a magnet to a mover
-function applyMagneticForce(magnet, mover) {
-  let force = p5.Vector.sub(magnet.position, mover.position);
-  let distance = force.mag();
-
-  if (distance > 0) {
-    force.setMag(magnetStrength / distance);
-    mover.applyForce(force);
+  display() {
+    fill(this.c);
+    circle(this.position.x, this.position.y, this.r);
   }
 }
